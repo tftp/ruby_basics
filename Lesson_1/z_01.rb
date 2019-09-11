8da23c9c8a3389a380226a8b59f5ea9f81abc460
Oleg Goloborodko <goloborodko@inbox.ru>
2019-09-11 17:50:06 +0300

    Lesson 1 correcting 2

--- Lesson_1/z_01.rb
+++ Lesson_1/z_01.rb
@@ -1,10 +1,10 @@
-#Программа расчитывает и выводит идеальный вес
-print "Введите свое имя: "
+# the program calculates and displays the ideal weight
+print 'Введите свое имя: '
 name = gets.chomp
-print "Введите свой рост: "
+print 'Введите свой рост: '
 growth = gets.chomp.to_i
 ideal_growth = growth - 110
-if ideal_growth > 0
+if ideal_growth.positive?
   puts "#{name}, ваш идеальный вес: #{ideal_growth} кг."
 else
   puts "#{name}, ваш вес уже оптимальный!"
