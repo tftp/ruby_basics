f280a7f33cbc0135516507462d6bfbc8feb1d3bb
Oleg Goloborodko <goloborodko@inbox.ru>
2019-09-15 13:06:56 +0300

    Lesson 2 corrected z_03 with while

--- Lesson_2/z_03.rb
+++ Lesson_2/z_03.rb
@@ -1,11 +1,10 @@
 fibonachy = [0, 1]
-i = 0
+i = 1
+next_num = 1
 
-loop do
-  next_num = fibonachy[i] + fibonachy[i + 1]
-  break if next_num > 100
-
+while next_num < 100
   fibonachy << next_num
+  next_num = fibonachy[i] + fibonachy[i + 1]
   i += 1
 end
 
